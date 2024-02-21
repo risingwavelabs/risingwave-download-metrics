8ce0a95860ea1bf20c8944574bc3e4b282d8d9e0
Noel Kwan <noelkwan1998@gmail.com>
2024-02-21 23:40:08 +0800

    use cli args for new record

--- update_data.py
+++ update_data.py
@@ -1,12 +1,22 @@
 #!/usr/bin/env python3
 
 import json
+import sys
 
 # Read json file
 with open('counts.json') as f:
     data = json.load(f)
-    # Push a new record into data.
-    data.append({"date": "2022-01-01", "count": 1})
+    # Get date from cli args
+    date = sys.argv[1]
+    osx_count = int(sys.argv[2])
+    linux_count = int(sys.argv[3])
+    new_record = {
+        "date": date,
+        "osx_count": osx_count,
+        "linux_count": linux_count
+    }
+    data.append(new_record)
+
     # Write the updated data back to the file.
     with open('counts.json', 'w') as f:
         json.dump(data, f)
