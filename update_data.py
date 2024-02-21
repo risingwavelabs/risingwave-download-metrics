c64f96a582e93e3eea8531227aba3468bb8859b6
Noel Kwan <noelkwan1998@gmail.com>
2024-02-21 23:28:07 +0800

    try update counts

---
+++ update_data.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import json
+
+# Read json file
+with open('counts.json') as f:
+    data = json.load(f)
+    # Push a new record into data.
+    data.append({"date": "2022-01-01", "count": 1})
+    # Write the updated data back to the file.
+    with open('counts.json', 'w') as f:
+        json.dump(data, f, indent=2)
