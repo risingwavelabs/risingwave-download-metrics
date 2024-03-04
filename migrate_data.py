e2295ed8884ca52a293d06b5fbb0c49b91b3e767
Noel Kwan <noelkwan1998@gmail.com>
2024-03-04 12:15:49 +0800

    update total counts by version

--- migrate_data.py
+++ migrate_data.py
@@ -8,7 +8,10 @@ with open('counts.json') as f:
     data = json.load(f)
     # Get date from cli args
     for record in data:
-        record['tag_name'] = "v1.6.1"
+        if record['date'] == "2024-03-01":
+            record['tag_name'] = "v1.7.0"t s
+        else:
+            record['tag_name'] = "v1.6.1"
 
     # Write the updated data back to the file.
     with open('counts.json', 'w') as f:
--- update_data.py
+++ update_data.py
@@ -4,16 +4,18 @@ import json
 import sys
 
 # Read json file
-with open('counts.json') as f:
+with open('count_versioned.json') as f:
     data = json.load(f)
     # Get date from cli args
     date = sys.argv[1]
     osx_count = int(sys.argv[2])
     linux_count = int(sys.argv[3])
+    tag_name = sys.argv[4]
     new_record = {
         "date": date,
         "osx_count": osx_count,
-        "linux_count": linux_count
+        "linux_count": linux_count,
+        "tag_name": tag_name
     }
     # Make sure the date is not already in the file
     for record in data:
@@ -23,5 +25,5 @@ with open('counts.json') as f:
     data.append(new_record)
 
     # Write the updated data back to the file.
-    with open('counts.json', 'w') as f:
+    with open('count_versioned.json', 'w') as f:
         json.dump(data, f)
