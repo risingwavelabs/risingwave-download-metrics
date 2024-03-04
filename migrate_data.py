1b71b5df15a8a46237325be8d97e9e674d6ac0b9
Noel Kwan <noelkwan1998@gmail.com>
2024-03-04 12:41:10 +0800

    Revert "update total counts by version"
    This reverts commit e2295ed8884ca52a293d06b5fbb0c49b91b3e767.

--- migrate_data.py
+++ migrate_data.py
@@ -8,10 +8,7 @@ with open('counts.json') as f:
     data = json.load(f)
     # Get date from cli args
     for record in data:
-        if record['date'] == "2024-03-01":
-            record['tag_name'] = "v1.7.0"t s
-        else:
-            record['tag_name'] = "v1.6.1"
+        record['tag_name'] = "v1.6.1"
 
     # Write the updated data back to the file.
     with open('counts.json', 'w') as f:
--- update_data.py
+++ update_data.py
@@ -4,18 +4,16 @@ import json
 import sys
 
 # Read json file
-with open('count_versioned.json') as f:
+with open('counts.json') as f:
     data = json.load(f)
     # Get date from cli args
     date = sys.argv[1]
     osx_count = int(sys.argv[2])
     linux_count = int(sys.argv[3])
-    tag_name = sys.argv[4]
     new_record = {
         "date": date,
         "osx_count": osx_count,
-        "linux_count": linux_count,
-        "tag_name": tag_name
+        "linux_count": linux_count
     }
     # Make sure the date is not already in the file
     for record in data:
@@ -25,5 +23,5 @@ with open('count_versioned.json') as f:
     data.append(new_record)
 
     # Write the updated data back to the file.
-    with open('count_versioned.json', 'w') as f:
+    with open('counts.json', 'w') as f:
         json.dump(data, f)
